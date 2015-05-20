db6fa19a633f697efd431136f3447944048e8fc8
Hacker-liang <pengshuai.liang@gmai.com>
2015-05-20 10:14:16 +0800

    优化 coredata 的结构

--- Freedom/Freedom/Cost.swift
+++ Freedom/Freedom/Cost.swift
@@ -2,17 +2,24 @@
 //  Cost.swift
 //  Freedom
 //
-//  Created by liangpengshuai on 5/19/15.
+//  Created by liangpengshuai on 5/20/15.
 //  Copyright (c) 2015 com.lps.www. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-
 class Cost: NSManagedObject {
 
     @NSManaged var costId: String
     @NSManaged var createTime: NSNumber
+    @NSManaged var budgetId: String
+    @NSManaged var type: NSNumber
+    @NSManaged var value: NSNumber
+    @NSManaged var lat: NSNumber
+    @NSManaged var lng: NSNumber
+    @NSManaged var note: String
+    @NSManaged var metadataId: String
+    @NSManaged var currency: NSNumber
 
 }
